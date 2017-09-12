48b926e1d8a0b1fe2d67823d98325a9fc7dee97c
gabidavila <gabidavila@gmail.com>
2017-09-12 10:24:09 -0400

    Rails API setup

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+Question.create([
+  { content: 'The frontend (Vanilla JS) and backend (Rails API) will reside in two separate repos'},
+  { content: 'Install and configure Postgresql <a target="_blank" href="https://postgresapp.com/">Postgresapp</a>.' },
+  { content: 'Remember to use "--database=postgresql" when generating your Rails application to use Postgresql vs SqlLite.' },
+  { content: 'Create your Rails application as an <a target="_blank" href="http://guides.rubyonrails.org/api_app.html">API</a>.' },
+  { content: 'Add support for <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Access_control_CORS">cross-origin resource sharing</a>, using <a target="_blank" href=" https://github.com/cyu/rack-cors">rack-cors</a>. See this <a target="_blank" href="https://github.com/learn-co-curriculum/js-project-week-setup/commit/8272dbfcde33503adbb22bb0dbc731233527fad6">commit</a> as a reference.' },
+  { content: 'Design your data model and define the structure of your JSON data.' },
+  { content: 'Remember to namespace your <a target="_blank" href="http://guides.rubyonrails.org/routing.html#controller-namespaces-and-routing">routes and controllers</a>.' },
+  { content: 'Learn <a href="https://chriskottom.com/blog/2017/04/versioning-a-rails-api"/>why</a> version an API'},
+  { content: 'When lost take a walk, then bifriend <a target="_blank" href="https://developer.mozilla.org/en-US/">MDN</a> and <a target="_blank" href="http://api.rubyonrails.org/">api.rubyonrails.org</a>.' }
+  ])
