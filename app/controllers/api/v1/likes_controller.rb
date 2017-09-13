f227048ce17f9b8b052896a58496b60c73086233
gabidavila <gabidavila@gmail.com>
2017-09-13 17:37:15 -0400

    swaps the points if clicked

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,11 +1,24 @@
 class Api::V1::LikesController < ApplicationController
 
   def create
-    @like = Like.create(like_params)
-    if @like.valid?
-      render json: @like, status: 201
+    # Swaps the points
+    @possibleLike = Like.find_by(user_id: like_params[:user_id], reply_id: like_params[:reply_id])
+
+    if @possibleLike && @possibleLike.point == like_params[:point]
+      @possibleLike.destroy
+      render json: [], status: 200
+    elsif @possibleLike
+      @possibleLike.point = like_params[:point]
+      @possibleLike.save
+      render json: @possibleLike, status: 200
     else
-      render json: [], status: 403
+      # Tries to create the new vote
+      @like = Like.create(like_params)
+      if @like.valid?
+        render json: @like, status: 201
+      else
+        render json: [], status: 403
+      end
     end
   end
 
